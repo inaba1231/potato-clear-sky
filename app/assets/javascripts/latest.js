a90113d2e233a610b61e7534c959788838f1271d
Inaba Kazuhiro <inaba.k94@gmail.com>
2016-06-22 21:49:38 +0800

    Add direction result panel

--- app/assets/javascripts/latest.js
+++ app/assets/javascripts/latest.js
@@ -89,8 +89,8 @@ function initMap() {
       return;
     } else {
       directionsService.route({
-        origin: origin_place_id.placeId,
-        destination: destination_place_id.placeId,
+        origin: origin_place_id,
+        destination: destination_place_id,
         travelMode: travel_mode,
         provideRouteAlternatives: true
       }, function(response, status) {
