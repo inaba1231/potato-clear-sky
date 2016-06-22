42e35eeab09fe97ca660784ea77f4c630065929d
Inaba Kazuhiro <inaba.k94@gmail.com>
2016-06-23 00:08:07 +0800

    Add direction result panel

--- app/assets/javascripts/latest.js
+++ app/assets/javascripts/latest.js
@@ -128,18 +128,27 @@ function initMap() {
        new google.maps.LatLng(1.4793, 104.1375), 
        new google.maps.LatLng(1.1508, 103.5521)
   );
-  
-  var lastValidCenter = map.getCenter();
 
-  google.maps.event.addListener(map, 'center_changed', function() {
-      if (allowedBounds.contains(map.getCenter())) {
-          // still within valid bounds, so save the last valid position
-          lastValidCenter = map.getCenter();
-          return; 
-      }
+  // Listen for the dragend event
+  google.maps.event.addListener(map, 'dragend', function() {
+    if (allowedBounds.contains(map.getCenter())) return;
 
-      // not valid anymore => return to last valid position
-      map.panTo(lastValidCenter);
+    // Out of bounds - Move the map back within the bounds
+
+    var c = map.getCenter(),
+         x = c.lng(),
+         y = c.lat(),
+         maxX = allowedBounds.getNorthEast().lng(),
+         maxY = allowedBounds.getNorthEast().lat(),
+         minX = allowedBounds.getSouthWest().lng(),
+         minY = allowedBounds.getSouthWest().lat();
+
+    if (x < minX) x = minX;
+    if (x > maxX) x = maxX;
+    if (y < minY) y = minY;
+    if (y > maxY) y = maxY;
+
+    map.setCenter(new google.maps.LatLng(y, x));
   });
 
 	/*
