48a1c21510c8007ed155cceec24745d7f226ee5c
Inaba Kazuhiro <inaba.k94@gmail.com>
2016-06-22 16:01:21 +0800

    Update direction with selected travel mode

--- app/assets/javascripts/latest.js
+++ app/assets/javascripts/latest.js
@@ -1,9 +1,6 @@
 function initMap() {
 
-  var origin_place_id = null;
-  var destination_place_id = null;
-  var travel_mode = google.maps.TravelMode.WALKING;
-
+  // Create base map.
 	var map = new google.maps.Map(document.getElementById('map'), {
     	zoom: 11,
 		  minZoom: 11,
@@ -11,38 +8,67 @@ function initMap() {
       mapTypeControl: false
   });
 
+  // Create direction service and direction display.
   var directionsService = new google.maps.DirectionsService;
   var directionsDisplay = new google.maps.DirectionsRenderer;
   directionsDisplay.setMap(map);
 
+  // HTML input elements.
   var origin_input = document.getElementById('origin-input');
   var destination_input = document.getElementById('destination-input');
   var modes = document.getElementById('mode-selector');
 
-  /*
-  map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(origin_input);
-  map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(destination_input);
-  map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(modes);
-  */
-
+  // Create autocomplete text inputs.
   var origin_autocomplete = new google.maps.places.Autocomplete(origin_input);
   origin_autocomplete.bindTo('bounds', map);
-  var destination_autocomplete =
-      new google.maps.places.Autocomplete(destination_input);
+  var destination_autocomplete = new google.maps.places.Autocomplete(destination_input);
   destination_autocomplete.bindTo('bounds', map);
 
-  // Sets a listener on a radio button to change the filter type on Places
-  // Autocomplete.
+  // Sets a listener on a radio button to change the travel mode.
+  var travel_mode = google.maps.TravelMode.WALKING;
+  var origin_place_id = null;
+  var destination_place_id = null;
+
   function setupClickListener(id, mode) {
     var radioButton = document.getElementById(id);
     radioButton.addEventListener('click', function() {
       travel_mode = mode;
     });
   }
+
   setupClickListener('changemode-walking', google.maps.TravelMode.WALKING);
   setupClickListener('changemode-transit', google.maps.TravelMode.TRANSIT);
   setupClickListener('changemode-driving', google.maps.TravelMode.DRIVING);
 
+  // Sets a listener to expand viewport and generate route when origin input is filled.  
+  origin_autocomplete.addListener('place_changed', function() {
+    var place = origin_autocomplete.getPlace();
+    if (!place.geometry) {
+      window.alert("Autocomplete's returned place contains no geometry");
+      return;
+    } else {
+      expandViewportToFitPlace(map, place);
+      origin_place_id = place.place_id;
+      route(origin_place_id, destination_place_id, travel_mode,
+            directionsService, directionsDisplay);
+    }
+  });
+
+  // Sets a listener to expand viewport and generate route when destination input is filled.
+  destination_autocomplete.addListener('place_changed', function() {
+    var place = destination_autocomplete.getPlace();
+    if (!place.geometry) {
+      window.alert("Autocomplete's returned place contains no geometry");
+      return;
+    } else {
+      expandViewportToFitPlace(map, place);
+      destination_place_id = place.place_id;
+      route(origin_place_id, destination_place_id, travel_mode,
+            directionsService, directionsDisplay);
+    }
+  });
+
+  // Expands viewport to fit the place origin or destination.
   function expandViewportToFitPlace(map, place) {
     if (place.geometry.viewport) {
       map.fitBounds(place.geometry.viewport);
@@ -52,57 +78,30 @@ function initMap() {
     }
   }
 
-  origin_autocomplete.addListener('place_changed', function() {
-    var place = origin_autocomplete.getPlace();
-    if (!place.geometry) {
-      window.alert("Autocomplete's returned place contains no geometry");
-      return;
-    }
-    expandViewportToFitPlace(map, place);
-
-    // If the place has a geometry, store its place ID and route if we have
-    // the other place ID
-    origin_place_id = place.place_id;
-    route(origin_place_id, destination_place_id, travel_mode,
-          directionsService, directionsDisplay);
-  });
-
-  destination_autocomplete.addListener('place_changed', function() {
-    var place = destination_autocomplete.getPlace();
-    if (!place.geometry) {
-      window.alert("Autocomplete's returned place contains no geometry");
-      return;
-    }
-    expandViewportToFitPlace(map, place);
-
-    // If the place has a geometry, store its place ID and route if we have
-    // the other place ID
-    destination_place_id = place.place_id;
-    route(origin_place_id, destination_place_id, travel_mode,
-          directionsService, directionsDisplay);
-  });
-
+  // Generates route from origin to destination in the selected travel mode.
   function route(origin_place_id, destination_place_id, travel_mode,
                  directionsService, directionsDisplay) {
     if (!origin_place_id || !destination_place_id) {
       return;
+    } else {
+      directionsService.route({
+        origin: {'placeId': origin_place_id},
+        destination: {'placeId': destination_place_id},
+        travelMode: travel_mode
+      }, function(response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
     }
-    directionsService.route({
-      origin: {'placeId': origin_place_id},
-      destination: {'placeId': destination_place_id},
-      travelMode: travel_mode
-    }, function(response, status) {
-      if (status === google.maps.DirectionsStatus.OK) {
-        directionsDisplay.setDirections(response);
-      } else {
-        window.alert('Directions request failed due to ' + status);
-      }
-    });
   }
-
   
+  // URL for latest heatmap.
   var heatmap_url = $("#map").data("heatmap");
 
+  // Singapore's bounds.
 	var bounds = {
     north: 1.4793,
     south: 1.1508,
@@ -110,13 +109,14 @@ function initMap() {
     west: 103.5521
   };
 
-	var historicalOverlay = new google.maps.GroundOverlay(heatmap_url,
+  // Overlay heatmap with base map.
+	var Overlay = new google.maps.GroundOverlay(heatmap_url,
 		bounds,
 		{
 			opacity: 1
 		});
-	historicalOverlay.setMap(map);
 
+	Overlay.setMap(map);
 
 	/*
 	var northeast = new google.maps.Marker({
